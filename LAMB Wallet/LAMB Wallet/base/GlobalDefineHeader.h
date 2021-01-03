d139cf13474cf755db8c93a664a78272f1a461b5
Sunny <songmin_ios@126.com>
2021-01-03 19:43:46 +0800

    请求地址修改

--- LAMB Wallet/LAMB Wallet/base/GlobalDefineHeader.h
+++ LAMB Wallet/LAMB Wallet/base/GlobalDefineHeader.h
@@ -11,12 +11,12 @@
 
 
 #ifdef DEBUG
-#define DEBUGBASEURL @"http://47.93.196.236:13659"
-#define RELEASEBASEURL @"http://39.107.247.86:13659"
+#define DEBUGBASEURL @"47.93.196.236:13659"
+#define RELEASEBASEURL @"39.107.247.86:13659"
 //#define RELEASEBASEURL @"39.107.247.86:13659"
 #else
-#define DEBUGBASEURL @"http://bj1.testnet.lambdastorage.com:13659"
-#define RELEASEBASEURL @"http://39.107.247.86:13659"
+#define DEBUGBASEURL @"bj1.testnet.lambdastorage.com:13659"
+#define RELEASEBASEURL @"39.107.247.86:13659"
 #endif
 
 #define lambAddress @"lambda1ymms062l3v55tyfkeqp605psvdh4za78k6ufcw"
